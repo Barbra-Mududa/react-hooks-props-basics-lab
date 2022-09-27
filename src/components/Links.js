f908c81aa8b00df7b6dbae99d5a413d75edc1923
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-27 13:01:03 +0300

    Links

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Links(){
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+    )
+}
